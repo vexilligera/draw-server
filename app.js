07ab26e3dca93f5ecfba107ecb6294848b3b45ce
wushuang.yoyo <1054096100@qq.com>
2020-09-21 20:41:41 +0800

    Delete line require module './route/save.js'

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const users = require('./routes/users')
 const config = require('./routes/config')
 const register = require('./routes/register')
 const file = require('./users/file')
-const save = require('./routes/save')
+// const save = require('./routes/save')
 
 // error handler
 onerror(app)
